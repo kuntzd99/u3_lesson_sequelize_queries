de8d47e2280b0a2506a612b4aada43545e1af4fc
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-08 13:25:32 -0500

    lesson complete

--- queries.js
+++ queries.js
@@ -5,8 +5,56 @@ const stringify = (data) => {
   console.log(JSON.stringify(data, null, 2))
 }
 
+const findAllBusinesses = async () => {
+  const result = await Business.findAll()
+  stringify(result)
+}
+
+const findBusinessById = async (id) => {
+  const result = await Business.findByPk(id)
+  stringify(result)
+}
+
+const findBusinessByRating = async (rating) => {
+  const result = await Business.findOne({ where: { rating: rating } })
+  stringify(result)
+}
+
+const createBusiness = async () => {
+  const result = await Business.create({
+    name: "Daniel's Business",
+    address: 'Hyde Park',
+    rating: 5
+  })
+  stringify(result)
+}
+
+const updateBusiness = async () => {
+  const result = await Business.update(
+    {
+      name: "Daniel's Better Business",
+      address: 'HydePark',
+      rating: 5
+    },
+    { where: { name: "Daniel's Business" } }
+  )
+  stringify(result)
+}
+
+const deleteBusiness = async () => {
+  await Business.destroy({
+    where: { name: "Daniel's Better Business" }
+  })
+}
+
 async function main() {
   try {
+    //await findAllBusinesses()
+    //await findBusinessById(4)
+    //await findBusinessByRating(5)
+    //await createBusiness()
+    //await updateBusiness()
+    //await deleteBusiness()
   } catch (error) {
     console.log(error)
   } finally {
